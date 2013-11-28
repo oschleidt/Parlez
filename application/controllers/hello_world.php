629c63609a556bb84913558e74cc9d2500dbbccf
Oliver Schleidt <oliverschleidt@gmail.com>
2013-11-29 00:32:15 +0100

    email

--- application/controllers/hello_world.php
+++ application/controllers/hello_world.php
@@ -16,13 +16,13 @@ class Hello_world extends CI_Controller {
         $this->email->message('Testing the email class.');
 
 
-        if($this->email->send())
+        /*if($this->email->send())
         {
             echo 'Email sent.';
         }
         else
         {
             show_error($this->email->print_debugger());
-        }
+        }*/
 	}
 }
