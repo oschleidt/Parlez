94f5cc7e8820b08dd397985fb30a58d56a91ec86
Oliver Schleidt <oliverschleidt@gmail.com>
2013-12-11 15:36:01 +0100

    Name geändert

--- application/views/banner.php
+++ application/views/banner.php
@@ -8,7 +8,7 @@ class test {
 
 <div id="banner">
     <div class="wrapper">
-        <a href="index.php" id="title">Name</a><br/>
+        <a href="index.php" id="title">Parlez</a><br/>
         <a href="index.php" id="subtitle"> - get connected</a>
         <div id="menu">
 
